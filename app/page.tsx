5dcc44df4090d932c49d374796736d9e90d6d993
jr-cast <castanos@protonmail.com>
2024-02-07 16:17:06 -0700

    chore: Update default canva hight value to correct one

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,52 @@
-"use client"
+"use client";
 // @ts-ignore
-import * as headbreaker from 'headbreaker';
-import { ChangeEventHandler, Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
+import * as headbreaker from "headbreaker";
+import {
+  ChangeEventHandler,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
-function DemoJigsaw(props: { id: string, width: number, height: number, pieceSize: number }) {
-  const puzzleRef = useRef(null)
-
+function DemoJigsaw(props: {
+  id: string;
+  width: number;
+  height: number;
+  pieceSize: number;
+}) {
+  const puzzleRef = useRef(null);
 
   useEffect(() => {
-    const puzzle = puzzleRef.current
+    const puzzle = puzzleRef.current;
     // @ts-ignore
     const canvas = new headbreaker.Canvas(puzzle.id, {
-      width: props.width, height: props.width,
-      pieceSize: props.pieceSize, proximity: props.pieceSize / 5,
-      borderFill: props.pieceSize / 10, strokeWidth: 2, lineSoftness: 0.18,
-      painter: new headbreaker.painters.Konva()
+      width: props.width,
+      height: props.height,
+      pieceSize: props.pieceSize,
+      proximity: props.pieceSize / 5,
+      borderFill: props.pieceSize / 10,
+      strokeWidth: 2,
+      lineSoftness: 0.18,
+      painter: new headbreaker.painters.Konva(),
     });
 
     canvas.autogenerate({
       horizontalPiecesCount: 2,
       verticalPiecesCount: 2,
       metadata: [
-        { color: '#B83361' },
-        { color: '#B87D32' },
-        { color: '#A4C234' },
-        { color: '#37AB8C' }
-      ]
+        { color: "#B83361" },
+        { color: "#B87D32" },
+        { color: "#A4C234" },
+        { color: "#37AB8C" },
+      ],
     });
 
     canvas.draw();
-  }, [
-    props.height, props.pieceSize, props.width, props.width
-  ])
+  }, [props.height, props.pieceSize, props.width, props.width]);
 
-  return <div ref={puzzleRef} id={props.id}></div>
+  return <div ref={puzzleRef} id={props.id}></div>;
 }
 
 export default function Home() {
@@ -41,21 +54,44 @@ export default function Home() {
   const [width, setWidth] = useState(600);
   const [height, setHeight] = useState(850);
 
-  function handleClick(f: Dispatch<SetStateAction<number>>) : ChangeEventHandler<HTMLInputElement> {
-    return (e) => f(Number(e.target.value))
+  function handleClick(
+    f: Dispatch<SetStateAction<number>>
+  ): ChangeEventHandler<HTMLInputElement> {
+    return (e) => f(Number(e.target.value));
   }
 
   return (
     <>
       <main className="">
         <h1>Headbreaker From React</h1>
-
-        <label htmlFor="piece-size">Piece Size</label> <input id="piece-size"  type="number" value={pieceSize} onChange={handleClick(setPieceSize)} />
-        <label htmlFor="weight">Width</label> <input id="weight" type="number" value={width} onChange={handleClick(setWidth)} />
-        <label htmlFor="height">Height</label> <input id="height" type="number" value={height} onChange={handleClick(setHeight)} />
-
-        <DemoJigsaw id="puzzle" pieceSize={pieceSize} width={width} height={height} />
+        <label htmlFor="piece-size">Piece Size</label>{" "}
+        <input
+          id="piece-size"
+          type="number"
+          value={pieceSize}
+          onChange={handleClick(setPieceSize)}
+        />
+        <label htmlFor="weight">Width</label>{" "}
+        <input
+          id="weight"
+          type="number"
+          value={width}
+          onChange={handleClick(setWidth)}
+        />
+        <label htmlFor="height">Height</label>{" "}
+        <input
+          id="height"
+          type="number"
+          value={height}
+          onChange={handleClick(setHeight)}
+        />
+        <DemoJigsaw
+          id="puzzle"
+          pieceSize={pieceSize}
+          width={width}
+          height={height}
+        />
       </main>
     </>
-  )
+  );
 }
